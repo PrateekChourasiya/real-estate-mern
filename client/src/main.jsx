d2e061e81f238b29078e77774c7772b95d16c4e5
Prateek <prateekchourasiya2003@gmail.com>
2025-07-29 00:50:11 +0530

    added redux persist to avoid the missing the userData when site refreshed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,17 @@
-import { StrictMode } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { store } from './redux/store.js'
+import { persistor } from './redux/store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store = {store}>
-    <App/>
+    <PersistGate loading = {null} persistor={persistor}>
+      <App/>
+    </PersistGate>
   </Provider>,
 )
