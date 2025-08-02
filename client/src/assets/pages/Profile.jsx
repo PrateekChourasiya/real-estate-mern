fb3f4b019603e00d3e9bc3ba9b06f025b587b439
Prateek <prateekchourasiya2003@gmail.com>
2025-08-02 16:51:09 +0530

    added upload file input while clicking the photo in profile page

--- client/src/assets/pages/Profile.jsx
+++ client/src/assets/pages/Profile.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useRef } from 'react';
 
 export default function Profile() {
   const {currentUser} = useSelector((state) => state.user);
+  const fileRef = useRef(null);
   return (
     <div className='p-3 max-w-lg mx-auto'>
        <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
        <form className='flex flex-col gap-4'>
-        <img src = {currentUser.avatar} alt = "Profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
+        <input type='file' ref = {fileRef} hidden accept='image/*'/>
+        <img src = {currentUser.avatar} alt = "Profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' onClick={()=> fileRef.current.click()}/>
         <input type = "text" placeholder='userName' className='border p-3 rounded-lg' id='userName'/>
         <input type = "email" placeholder='emailID' className='border p-3 rounded-lg' id='emailID'/>
         <input type = "password" placeholder='password' className='border p-3 rounded-lg' id='password'/>
